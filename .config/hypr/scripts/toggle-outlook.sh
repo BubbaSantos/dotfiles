afd9d36c9c0660fc295d912664caf36d94c7f7da
BubbaSantos <ultimatessambbo@gmail.com>
2026-03-02 16:30:41 +0000

    dotfiles: 2026-03-02 16:30

--- .config/hypr/scripts/toggle-outlook.sh
+++ .config/hypr/scripts/toggle-outlook.sh
@@ -3,18 +3,9 @@ ADDRESS=$(hyprctl clients -j | jq -r '.[] | select(.class | contains("outlook.of
 if [ -n "$ADDRESS" ]; then
     hyprctl dispatch focuswindow "address:$ADDRESS"
 else
-    # Check if system prefers dark mode
-    COLOR_SCHEME=$(gsettings get org.gnome.desktop.interface color-scheme)
-    
-    if [[ "$COLOR_SCHEME" == *"dark"* ]]; then
-        EXTRA_FLAGS="--force-dark-mode --enable-features=WebContentsForceDark"
-    else
-        EXTRA_FLAGS=""
-    fi
-    
     chromium --profile-directory="Profile 1" --app="https://outlook.office.com" \
         --hide-scrollbars \
         --enable-features=UseOzonePlatform,WebAppWindowControlsOverlay \
         --ozone-platform=wayland \
-        $EXTRA_FLAGS &
+        --disable-features=WaylandWpColorManagerV1,WebContentsForceDark &
 fi
