250239f9e8b193f327dbf40a17cddce6ad0e6e5d
BubbaSantos <ultimatessambbo@gmail.com>
2026-03-31 11:58:54 +0100

    dotfiles: 2026-03-31 11:58

--- .config/hypr/scripts/toggle-todo.sh
+++ .config/hypr/scripts/toggle-todo.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-APP_CLASS="chrome-to-do.office.com__tasks_inbox-Profile_1"
-
+APP_CLASS="chrome-to-do.office.com__tasks_inbox-Default"
 if hyprctl clients -j | grep -q "\"class\": \"$APP_CLASS\""; then
     hyprctl dispatch togglespecialworkspace todo
 else
-    hyprctl dispatch exec "[workspace special:todo silent]" "google-chrome-stable --profile-directory='Profile 1' --app=https://to-do.office.com/tasks/inbox"
+    hyprctl dispatch exec "[workspace special:todo silent]" "chromium --profile-directory='Default' --app=https://to-do.office.com/tasks/inbox"
+    # Wait for the window to appear, then toggle to it
+    for i in $(seq 1 20); do
+        sleep 0.3
+        if hyprctl clients -j | grep -q "\"class\": \"$APP_CLASS\""; then
+            hyprctl dispatch togglespecialworkspace todo
+            break
+        fi
+    done
 fi
