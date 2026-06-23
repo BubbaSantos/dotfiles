efb7e5f0d80d0dc2e6501ab2b20f2e8f7627778c
BubbaSantos <ultimatessambbo@gmail.com>
2026-06-23 08:00:16 +0100

    dotfiles: 2026-06-23 08:00

--- .config/hypr/scripts/close_other_workspaces.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-current=$(hyprctl activeworkspace -j | jq '.id')
-hyprctl clients -j | jq -r ".[] | select(.workspace.id != $current) | .address" | \
-  while read addr; do
-    hyprctl dispatch closewindow "address:$addr"
-  done
